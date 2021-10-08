b0e093f3220a918694a63867aa38bd221348a8d3
Gareth Jones <jones258@gmail.com>
2021-10-09 11:25:33 +1300

    feat: setup documentation generation

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"terraform-provider-statuscake/statuscake"
 )
 
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	var debugMode bool
 
